a017b8276eb15deee2429a913fa58783de04bc85
qeqar <mark.eisenblaetter@gmail.com>
2019-09-20 16:18:31 +0200

    fist version of a message producer
    most values hardcoded
    reverse dns is needed so add ip/kafka host to /etc/hosts

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -1,1 +1,39 @@
-package publisher
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"log"
+	"os"
+)
+
+const (
+	brokers = "172.18.0.3:9092" // add 172.18.0.3 kafka to your host file
+	topic = "test"
+	message = "test message"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.Timeout = 10
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+
+	producer, err := sarama.NewSyncProducer([]string{brokers}, config)
+	if err != nil {
+		log.Panicf("Error creating Producer %v", err)
+		os.Exit(1)
+	}
+
+	msg := &sarama.ProducerMessage {
+        Topic: topic,
+        Value: sarama.StringEncoder(message),
+    }
+    p, o, err := producer.SendMessage(msg)
+    if err != nil {
+        fmt.Println("Error publish: ", err.Error())
+    }
+
+    fmt.Println("Partition: ", p)
+    fmt.Println("Offset: ", o)
+}
